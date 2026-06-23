6c5ffd99a656779ae5a8db858fc99a560f453b87
Olivia <oliviahan2308@gmail.com>
2026-06-23 13:04:07 -0500

    Create databse and database call with exception

--- webapp/App/views/__init__.py
+++ webapp/App/views/__init__.py
@@ -1,2 +1,3 @@
 """Views, one for each App's page."""
 from App.views.index import show_index
+from App.views.images import download_file
--- webapp/App/views/index.py
+++ webapp/App/views/index.py
@@ -11,5 +11,19 @@ import App
 @App.app.route('/')
 def show_index():
     """Display / route."""
+        # Connect to database
+    connection = App.model.get_db()
+
+    # Query database
+    logname = "John Doe"
+    cur = connection.execute(
+        "SELECT fullname, email, picture "
+        "FROM developer "
+        "WHERE fullname != ?",
+        (logname,)
+    )
+    devs = cur. fetchall()
+
+    
     context = {}
     return flask.render_template("index.html", **context)
